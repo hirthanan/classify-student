cfffea2bcd73a1c0eda0660dbc05525fef6d12bc
Hirthanan Subenderan <Hirthanan_97@hotmail.com>
2017-11-03 22:37:12 -0400

    corrected input to scikit tree function

--- classify_grade.py
+++ classify_grade.py
@@ -10,12 +10,12 @@ class School:
 
 # format: measures = [studying_per_day, sleep_per_day, university]
 measures = [[8, 6, School.WATERLOO], [5, 4, School.WATERLOO], [1, 9, School.LAURIER],
-            [5, 8, School.LAURIER], [6, 4, School.UOFT], [3, 10, School.LAURIER],
+            [5, 8, School.LAURIER], [5, 8, School.UOFT], [3, 10, School.LAURIER],
             [8, 3, School.UOFT], [9, 9, School.UOFT], [4, 8, School.WATERLOO],
             [5, 8, School.LAURIER], [6, 4, School.UOFT], [3, 10, School.LAURIER],
             [2,5, School.WATERLOO], [2, 4, School.UOFT], [3, 4, School.WATERLOO]]
 
-grade_level = ['outstanding', 'superior', 'good', 'outstanding',
+grade_level = ['outstanding', 'good', 'good', 'outstanding',
                 'superior', 'good', 'superior', 'outstanding',
                 'good', 'outstanding', 'superior', 'superior',
                 'satisfactory', 'satisfactory', 'satisfactory']
@@ -32,5 +32,17 @@ print "Enter students data in format : studying_per_day, sleep_per_day, universi
 
 for line in sys.stdin:
     student = line.split(' ')
+    num_of_stdy = int(student[0])
+    num_of_slp = int(student[1])
+    school = str(student[2][:-1])
+    uni_code = None
+    if school == "waterloo":
+        uni_code = 0
+    elif school == "laurier":
+        uni_code = 1
+    elif school == "uoft":
+        uni_code = 2
+    student = [num_of_stdy, num_of_slp, uni_code]
+    print student
     predict_grade_level = classifier.predict([student])
     print predict_grade_level
